ca307e73191da74fc6d423cb3c503a0a1003af05
Rodrigo Franca <rodrigo.franca@flexpag.com>
2020-11-21 20:08:19 -0300

    server.js

--- server.js
+++ server.js
@@ -9,4 +9,6 @@ app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + '/angularapp/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, function (){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
